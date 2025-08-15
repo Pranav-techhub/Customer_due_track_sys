df7e9d1d8b0048986d14e5ad811bbb19700a963c
Pranav-techhub <pranav.24sk@gmail.com>
2025-08-15 09:26:52 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,6 @@
-from flask import Flask
-from .routes import bp
+from backend import create_app
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(bp)
-    return app
+app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=True)
